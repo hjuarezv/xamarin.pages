d107e17432973d40e9c055a5c7539de9019bcb0e
hjuarezv <hjuarezx@gmail.com>
2022-11-05 21:40:40 -0500

    Proyecto de Xamarin.Forms sobre el uso de diferntes tipos de Pages y Layouts, Sesión Sabado 5 noviembre 2022

---
+++ HolaXamarin/HolaXamarin/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HolaXamarin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            //MainPage = new Page1();
+            //MainPage = new FlyoutPage1();
+            //MainPage = new NavigationPage( new NavigationDemo() );
+            //MainPage = new TabbedPage1();
+            MainPage = new CarouselDemo();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ HolaXamarin/HolaXamarin/NavigationDemo.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HolaXamarin
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NavigationDemo : ContentPage
+    {
+        public NavigationDemo()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page1());
+        }
+    }
+}
